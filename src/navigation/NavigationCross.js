a7cc8f02a38f7f868dd1ec01e295fec788a0af1e
Eric Dietzsch <eric.dietzsch@gmail.com>
2020-12-14 10:56:56 +0100

    Logic for NavigationCross

--- src/navigation/NavigationCross.js
+++ src/navigation/NavigationCross.js
@@ -6,10 +6,43 @@ import downArrow from '../icons/down-arrow.gif';
 import './NavigationCross.css';
 
 export default class NavigationCross extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movement:'',
+            targetView:'',
+            command:''
+        }
+    }
+
+    onAnimationEnd = () => {
+        setTimeout(() => {
+            if(this.state.targetView && this.state.targetView === 'room') {
+                this.props.handleRoomChange();
+            } else if(this.state.targetView && this.state.targetView.length !== 0) {
+                this.props.handleViewChange(this.state.targetView);
+            } else if(this.state.command && this.state.command.length !== 0) {
+                this.props.handleStateChange(this.state.command);
+            }
+            // reset values for next change
+            this.setState({movement:'', targetView:'', change:''});
+        }, 500);
+    }
+
+    // TODO: Needs further refinement ot avoid memory leaks at later points
+    // SEE: https://stackoverflow.com/questions/53949393/cant-perform-a-react-state-update-on-an-unmounted-component
+    /*
+    componentWillUnmount() {
+        // fix Warning: Can't perform a React state update on an unmounted component
+        this.setState = (state,callback)=>{
+            return;
+        };
+    }
+    */
+
     render(){
         let buttons = [];
         if (this.props.up != null) {
-            buttons.push(<img src={upArrow} className="up-arrow"/>);
             buttons.push(<div className="up-arrow">{this.props.up}</div>);
         } else {
             buttons.push(<img src={upArrow} className="up-arrow invisible"/>);
@@ -39,6 +72,9 @@ export default class NavigationCross extends React.Component {
         return (
             <div className="nav-cross">
                 {buttons}
+                <div className="nav-middle"><svg width="150" height="150">
+                    <rect onAnimationEnd={this.onAnimationEnd} className={this.state.movement} width="50" height="50" x="50" y="50" />
+                </svg></div>
             </div>
         )
     }
