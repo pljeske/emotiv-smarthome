353f224ca789d16905395a9a4a4ab42b28357639
Peer-Lucas Jeske <jeskep@th-brandenburg.de>
2020-12-17 07:48:59 +0100

    added router to make assistance/config page possible

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import Main from "./navigation/Main";
+import AssistanceSection from "./assistance/AssistanceSection";
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Main />
+          <Router>
+              <Switch>
+                  <Route path="/assistance">
+                      <AssistanceSection/>
+                  </Route>
+                  <Route path="/">
+                      <Main />
+                  </Route>
+              </Switch>
+          </Router>
       </header>
     </div>
   );
---
+++ src/assistance/AssistanceSection.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class AssistanceSection extends React.Component {
+    render(){
+        return <p>ASSISTANCE</p>;
+    }
+}
